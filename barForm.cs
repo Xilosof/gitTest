267031c070ad9ea7effdbd6dcb4dd7e6a9c68f9c
Xilosof <pro100temi444@gmail.com>
2019-03-31 17:48:51 +0300

    added using

--- barForm.cs
+++ barForm.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Threading;
-
+using System.Threading;
 namespace Lab2
 {
     public partial class barForm : Form
